fbd1cb7acd4752c07deb09461926a1f2e84641e1
jayrajsingh65 <singhjayraj69@gmail.com>
2024-10-07 15:14:21 +0530

    add hash password and user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
+import userModel from "./userModel";
+import { nextTick } from "process";
 
-const createUser = (req: Request,res:Response,next: NextFunction) => {
+const createUser = async (req: Request,res:Response,next: NextFunction) => {
 
     const {name, email, password} = req.body;
 
@@ -13,10 +16,23 @@ const createUser = (req: Request,res:Response,next: NextFunction) => {
 
 
 
+    const user = await userModel.findOne({email});
+    
+    if(user){
+        const error = createHttpError(400, "user already exits!")
+    
+        return next(error)
+    };
+
+
+    const hashedPassword = bcrypt.hash(password, 10)
 
     res.json({message: "User Created"})
 
 };
+
+
+
 
 
 export {createUser};
