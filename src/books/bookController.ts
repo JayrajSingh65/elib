632b5ba3b597fb9f9915c089463badb87c036a0b
jayrajsingh65 <singhjayraj69@gmail.com>
2024-10-15 14:02:29 +0530

    create newbook

--- src/books/bookController.ts
+++ src/books/bookController.ts
@@ -3,12 +3,11 @@ import express, { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import fs from "node:fs"
 import path from "node:path";
-import { json } from "node:stream/consumers";
 import cloudinary from "../../config/cloudinary";
 import bookModel from "./bookModel";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-    const {title, description, author, gener} = req.body;
+    const {title, gener,description} = req.body;
 
     console.log("files", req.files);
     const files = req.files as {[fieldname: string]: Express.Multer.File[]}
@@ -29,7 +28,7 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     
         const bookfileName = files.filelink[0].filename;
     
-        const bookFilePath = path.resolve(__dirname, "../../public/data/upload", bookfileName);
+        var bookFilePath = path.resolve(__dirname, "../../public/data/upload", bookfileName);
     
         const bookFileUploadResult = await cloudinary.uploader.upload(bookFilePath,{
             resource_type: 'raw',
@@ -42,22 +41,25 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         const newBook = await bookModel.create({
             title,
             gener,
+            description,
             author: "6703b2bbef591872a2405694",
             coverImage: uploadResult.secure_url,
             filelink: bookFileUploadResult.secure_url
 
         });
 
-        //delete file from server
         await fs.promises.unlink(filePath);
         
         await fs.promises.unlink(bookFilePath);
 
-
         res.status(201).json({id: newBook._id})
     
     } catch (error) {
-        return next(createHttpError(500, "Error while uploading files"))
-    }
+        // return next(createHttpError(500, "Error while uploading files"));
+        console.log(error)
+        
+    };
+
+    
 };
 export default createBook;
